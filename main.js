be7cb141dfc69625ffdb489797a353f662835eef
shuhrat <shyhratyagnobi2002@gmail>
2025-09-03 19:48:52 +0300

    practic in switch ternarn operator

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-console.log(6 - "2");
-console.log("3" + 2 - 1);
-console.log(6 - "2");
-let x = 1;
-x++;
-console.log(x);
-let y = 1;
-y--;
-console.log(y);
+// console.log(6 - "2");
+// console.log("3" + 2 - 1);
+// console.log(6 - "2");
+// let x = 1;
+// x++;
+// console.log(x);
+// let y = 1;
+// y--;
+// console.log(y);
 
-console.log(++y);
-let counter = 1;
+// console.log(++y);
+// let counter = 1;
 
-console.log(2 * ++counter);
-console.log(2 * counter++);
+// console.log(2 * ++counter);
+// console.log(2 * counter++);
+
+// let hi = "He said,  'hello!' ";
+// console.log(0 === false);
+
+let day = "s";
+switch (day) {
+  case "monday":
+    console.log("today  is monday ");
+
+    break;
+
+  default:
+    console.log("invalid day");
+
+    break;
+}
